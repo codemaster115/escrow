eba50469175f1e1ae9a33c00c0896041faec139d
codemaster115 <punctualityman@outlook.com>
2022-04-05 00:11:28 -0500

    add deposit,withdraw function

--- programs/test-escrow/src/lib.rs
+++ programs/test-escrow/src/lib.rs
@@ -1,15 +1,135 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount, Transfer},
+};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4B2qNhMFbFtABusdne83HDZmF3Ze8QDWTbnNYcKmaE4C");
 
 #[program]
 pub mod test_escrow {
+    use anchor_spl::token::transfer;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.mint = ctx.accounts.mint.key();
+        escrow.bump = *ctx.bumps.get("escrow").unwrap();
+
+        transfer(ctx.accounts.into(), amount)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.escrow_token.to_account_info(),
+            to: ctx.accounts.user_token.to_account_info(),
+            authority: ctx.accounts.escrow.to_account_info(),
+        };
+
+        let mint = ctx.accounts.mint.key();
+        let seeds = &[mint.as_ref(), &[ctx.accounts.escrow.bump]];
+        let cpi_context =
+            CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+
+        transfer(cpi_context.with_signer(&[seeds]), amount)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(amount: u64)]
+pub struct Deposit<'info> {
+    #[account(
+        init_if_needed,
+        payer = authority,
+        space = 8 + Escrow::LEN,
+        seeds = [mint.key().as_ref()],
+        bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = escrow
+    )]
+    pub escrow_token: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = authority,
+        constraint = user_token.amount >= amount @ TransferError::NotEnoughBalance
+    )]
+    pub user_token: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+impl<'info> From<&mut Deposit<'info>> for CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+    fn from(accounts: &mut Deposit<'info>) -> Self {
+        let cpi_accounts = Transfer {
+            from: accounts.user_token.to_account_info(),
+            to: accounts.escrow_token.to_account_info(),
+            authority: accounts.authority.to_account_info(),
+        };
+
+        CpiContext::new(accounts.token_program.to_account_info(), cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(amount: u64)]
+pub struct Withdraw<'info> {
+    #[account(
+        mut,
+        seeds = [mint.key().as_ref()],
+        bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        mut,
+        associated_token::mint = escrow.mint,
+        associated_token::authority = escrow,
+        constraint = escrow_token.amount >= amount @ TransferError::NotEnoughBalance
+    )]
+    pub escrow_token: Account<'info, TokenAccount>,
+    #[account(address = escrow.mint)]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = authority
+    )]
+    pub user_token: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Escrow {
+    mint: Pubkey,
+    bump: u8,
+}
+
+impl Escrow {
+    pub const LEN: usize = 32 + 1;
+}
+
+#[error_code]
+enum TransferError {
+    #[msg("Not enough balance in account")]
+    NotEnoughBalance,
+}
--- tests/test-escrow.ts
+++ tests/test-escrow.ts
@@ -1,6 +1,28 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
+import {
+  associatedAddress,
+  ASSOCIATED_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@project-serum/anchor/dist/cjs/utils/token";
+import { SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { TestEscrow } from "../target/types/test_escrow";
+
+let mint1 = new anchor.web3.PublicKey(
+  "AgSXZJCG9rmj6zcamhtZsiHzoJyqvVQWGXp6BcdGCxVN"
+);
+let mint2 = new anchor.web3.PublicKey(
+  "D6dTgQ5Tz6fodXxFvs51Y2EPiFxiV6abE7fW5wZgimDj"
+);
+let user1 = anchor.web3.Keypair.fromSecretKey(
+  new Uint8Array([
+    48, 55, 7, 132, 135, 100, 49, 66, 167, 93, 221, 229, 253, 96, 160, 57, 178,
+    27, 111, 129, 222, 104, 177, 90, 111, 201, 107, 64, 163, 187, 32, 102, 94,
+    105, 34, 183, 88, 218, 73, 76, 250, 24, 21, 92, 43, 43, 237, 126, 55, 129,
+    71, 26, 192, 99, 84, 117, 99, 50, 249, 237, 127, 196, 232, 208,
+  ])
+);
 
 describe("test-escrow", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +30,139 @@ describe("test-escrow", () => {
 
   const program = anchor.workspace.TestEscrow as Program<TestEscrow>;
 
-  it("Is initialized!", async () => {
+  it("Deposit Token1 Success!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const amount = new anchor.BN(10000);
+    const [escrow] = findProgramAddressSync(
+      [mint1.toBuffer()],
+      program.programId
+    );
+    const userToken = await associatedAddress({
+      mint: mint1,
+      owner: anchor.getProvider().wallet.publicKey,
+    });
+    const escrowToken = await associatedAddress({
+      mint: mint1,
+      owner: escrow,
+    });
+    let balance = await anchor
+      .getProvider()
+      .connection.getTokenAccountBalance(userToken);
+    console.log("Token balance: ", balance.value.uiAmountString);
+    const tx = await program.methods
+      .deposit(amount)
+      .accounts({
+        escrow,
+        escrowToken,
+        mint: mint1,
+        userToken,
+        authority: anchor.getProvider().wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Deposit Token2 Success!", async () => {
+    const amount = new anchor.BN(10000);
+    const [escrow] = findProgramAddressSync(
+      [mint2.toBuffer()],
+      program.programId
+    );
+    const userToken = await associatedAddress({
+      mint: mint2,
+      owner: user1.publicKey,
+    });
+    const escrowToken = await associatedAddress({
+      mint: mint2,
+      owner: escrow,
+    });
+    console.log(
+      "Token balance: ",
+      (await anchor.getProvider().connection.getTokenAccountBalance(userToken))
+        .value.uiAmountString
+    );
+    const tx = await program.methods
+      .deposit(amount)
+      .accounts({
+        escrow,
+        escrowToken,
+        mint: mint2,
+        userToken,
+        authority: user1.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .signers([user1])
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Withdraw Token1 to user1:", async () => {
+    const amount = new anchor.BN(10000);
+    const [escrow] = findProgramAddressSync(
+      [mint1.toBuffer()],
+      program.programId
+    );
+    const userToken = await associatedAddress({
+      mint: mint1,
+      owner: user1.publicKey,
+    });
+    const escrowToken = await associatedAddress({
+      mint: mint1,
+      owner: escrow,
+    });
+    const tx = await program.methods
+      .withdraw(amount)
+      .accounts({
+        escrow,
+        escrowToken,
+        mint: mint1,
+        userToken,
+        authority: user1.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .signers([user1])
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Withdraw Token2 to user:", async () => {
+    const amount = new anchor.BN(10000);
+    const [escrow] = findProgramAddressSync(
+      [mint2.toBuffer()],
+      program.programId
+    );
+    const userToken = await associatedAddress({
+      mint: mint2,
+      owner: anchor.getProvider().wallet.publicKey,
+    });
+    const escrowToken = await associatedAddress({
+      mint: mint2,
+      owner: escrow,
+    });
+    const tx = await program.methods
+      .withdraw(amount)
+      .accounts({
+        escrow,
+        escrowToken,
+        mint: mint2,
+        userToken,
+        authority: anchor.getProvider().wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
